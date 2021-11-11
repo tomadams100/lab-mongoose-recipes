e3fe4235216764d94a2077937f05f597d2ae0a64
Tom Adams <thomasjeadams@btinternet.com>
2021-11-11 17:59:43 +0100

    finished lab

--- index.js
+++ index.js
@@ -8,20 +8,46 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+const fillDataBase = async () => {
+  try {
+    await mongoose
+    .connect(MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log(`Connected to the database"`);
+     Recipe.deleteMany()
+
+    const recipe =
+    {
+      title: "Lasagna",
+      level: "Amateur Chef",
+      ingredients: ["pasta","beef","cheese", "tomato sauce"],
+      cuisine: "Italian",
+      dishType: "main_course",
+      image: "https://thestayathomechef.com/wp-content/uploads/2017/08/Most-Amazing-Lasagna-2-e1574792735811.jpg",
+      duration: 60,
+      creator: "Tom & Dani"
+    }
+
+    const createdRecipe = await Recipe.create(recipe);
+    console.log(createdRecipe); 
+
+    const createdRecipe = await Recipe.insertMany(data)
+   const updateRecipe = await Recipe.findOneAndUpdate(
+      {title: "Rigatoni alla Genovese"},
+      {duration: 100}
+    ) 
+    const deleteReceipe = await Recipe.deleteOne(
+      {title:"Carrot Cake"}
+    )
+  }
+  catch (error) {
+    console.log(error)
+  }
+  console.log("Disconnecting from the database...");
+  mongoose.disconnect();
+}
+fillDataBase()
+  
